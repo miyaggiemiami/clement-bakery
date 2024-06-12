2f27bf7418690899074143658ded76651c5e5765
miyaggiemiami <clementmorare971@gmail.com>
2024-06-12 22:21:26 +0200

    fixed bugs and imported errors

---
+++ src/assets/assets.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import logo from './logo.png'
+import applepie from './applepie.jpeg'
+import Bakedrolls from './Bakedrolls.jpeg'
+import banoffeepie from './banoffeepie.jpeg'
+import behindthescene from './behindthescene.jpeg'
+import brownbiscuit from './brownbiscuit.avif'
+import cakeberry from './cakeberry.jpeg'
+import cakeblackberry from './cakeblackBerry.jpeg'
+import cakeblackGrape from './cakeblackGrape.jpeg'
+import cakeMelted from './cakeMelted.jpeg'
+import cakenut from './cakenut.jpeg'
+import cherrypie from './cherrypie.png'
+import cookies from './cookies.jpeg'
+import flowerbiscuit from './flowerbiscuit.avif'
+import muffins from './muffins.avif'
+import muffin2 from './muffin2.avif'
+import muffin3 from './muffin3.jpeg'
+import snailrolls from './snailrolls.jpeg'
+import tomatoepizza from './tomatoepizza.jpeg'
+
+export default {
+    logo,
+    applepie,
+    Bakedrolls,
+    banoffeepie,
+    behindthescene,
+    brownbiscuit,
+    cakeberry,
+    cakeblackberry,
+    cakeblackGrape,
+    cakeMelted,
+    cakenut,
+    cherrypie,
+    cookies,
+    flowerbiscuit,
+    muffins,
+    muffin2,
+    muffin3,
+    snailrolls,
+    tomatoepizza
+};
+
