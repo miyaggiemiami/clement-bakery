92940c89ece81622a89b406955c1516c32133600
miyaggiemiami <clementmorare971@gmail.com>
2024-06-13 22:07:51 +0200

    added a background image inside the header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
     <div>
-        <header />
+        <Header />
     </div>
   )
 }
