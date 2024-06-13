319d513ae0ff5784e0bfa46bb1542fd492e002c7
miyaggiemiami <clementmorare971@gmail.com>
2024-06-13 20:14:39 +0200

    added handleMenuClick

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import './Navbar.css'
-import  assets from '../../assets/assets'
-
-
+import React, { useState } from 'react';
+import './Navbar.css';
+import assets from '../../assets/assets';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState('Home');
+
+  const handleMenuClick = (newMenu) => {
+    setMenu(newMenu);
+  };
+
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="clementaste-logo" className='logo'/>
-        <ul className="navbar-menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+      <img src={assets.logo} alt="clementaste-logo" className='logo'/>
 
-        <div className="navbar-right">
-            <img src={assets.search} alt="" className='search-icon' />
-            <div className="navbar-search-icon">
-                <img src={assets.basket} alt="" className='basket-icon'/>
-                <div className="dot"></div>
-            </div>
+      <ul className="navbar-menu">
+        <li className={menu === "Home" ? "active" : ""} onClick={() => handleMenuClick("Home")}>Home</li>
+        <li className={menu === "Menu" ? "active" : ""} onClick={() => handleMenuClick("Menu")}>Menu</li>
+        <li className={menu === "About" ? "active" : ""} onClick={() => handleMenuClick("About")}>About</li>
+        <li className={menu === "Contact" ? "active" : ""} onClick={() => handleMenuClick("Contact")}>Contact</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search} alt="" className='search-icon' />
+        <div className="navbar-search-icon">
+          <img src={assets.basket} alt="" className='basket-icon'/>
+          <div className="dot"></div>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
