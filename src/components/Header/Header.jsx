cef8b931dea8bf32afac744af6a2c66d15cc03e3
miyaggiemiami <clementmorare971@gmail.com>
2024-06-13 22:47:54 +0200

    imported  google font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <div className="header-content">
+        <div className="headerContent">
             <h2>Give Your Taste Buds A Clementaste</h2>
             <p>Our expert bakers combine the finest ingredients to create unique flavor profiles that will leave you wanting more. Try our signature blends today!</p>
             <button>Tasty Menu</button>
