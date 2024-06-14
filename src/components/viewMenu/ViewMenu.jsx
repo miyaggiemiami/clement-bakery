8b02c3d829c6066a6afd7924b4349701a16ed7ea
miyaggiemiami <clementmorare971@gmail.com>
2024-06-14 21:03:40 +0200

    added the logo inside the home page . added lines on the side of the logo

--- src/components/viewMenu/ViewMenu.jsx
+++ src/components/viewMenu/ViewMenu.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './ViewMenu.css'
-import { menu_list } from '../../assets/assets'
+import assets, { menu_list } from '../../assets/assets'
 
 const ViewMenu = () => {
     return (
       <div className='view-menu' id='view-menu'>
+        <div className="home-logo-container">
+        <img src={assets.logo}alt="Logo" className='home-logo'/>
+        <div className="vertical-line left"></div>
+            <div className="vertical-line right"></div>
+          </div>
           <h1>View Our Menu </h1>
-          <p className='view-menu-text'>dont just look, just buy </p>
+          <p className='view-menu-text'> Taste the freshness! Our bakery uses only the best ingredients to make our treats</p>
           <div className='view-menu-list'>
               {menu_list.map((item, index) => {
                   return(
