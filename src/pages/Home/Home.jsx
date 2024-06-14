99cb0e2a21c53f8508c49cb2a322baea09f487be
miyaggiemiami <clementmorare971@gmail.com>
2024-06-14 23:19:39 +0200

    added component that wraps the applicated and exported

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ViewMenu from '../../components/viewMenu/ViewMenu'
 
 const Home = () => {
+
+  const [category,setCategory] = useState("All");
   return (
     <div>
         <>
         <Header />
-        <ViewMenu />
+        <ViewMenu category={category} setCategory={setCategory} />
         </>
     </div>
   )
