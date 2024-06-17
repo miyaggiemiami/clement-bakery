28ea43dfe89b8fc4e5f669b92508d440d88a6a30
miyaggiemiami <clementmorare971@gmail.com>
2024-06-18 01:22:23 +0200

    footer

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FoodItem.css';
 
 const FoodItem = ({ id, name, price, description, image }) => {
+
+    const [itemCount,setItemCount] = useState(0)
+
   return (
     <div className='food-item'>
       <div className="food-item-container">
         <img className='food-item-img' src={image} alt={name} />
+        {!itemCount
+           ?<img src='assets.add_icon_white'
+        }
         <p>{name}</p>
       </div>
       <p className="food-item desc">{description}</p>
